306558ca1b68c6b03cdf9ec1b0e3722170a7e75f
Admin <Admin@LAPTOP-D38FQ3IJ>
2022-01-04 15:28:25 +0530

    commit first

---
+++ genkins/src/test/java/genkins/Testcase_01.java
@@ -0,0 +1,14 @@
+package genkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase_01 {
+
+	
+@Test
+public void CreatGenkins()
+{
+	Reporter.log("Create account");
+}
+}
